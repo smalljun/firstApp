b0c621d42280b697a9d229dd454b5f13f1848d86
smalljun <b9056005@gmail.com>
2016-04-30 10:55:27 +0800

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by 廖健宇 on 2016/4/30.
+//  Copyright © 2016年 廖健宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
